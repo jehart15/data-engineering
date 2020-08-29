f411235c62c5d3f330110da0306de7a75729620b
Jaynee Hart <jehart15@gmail.com>
2020-08-29 16:02:39 -0400

    for Jonathan's reading pleasure

---
+++ SFTP_download_jpegs.py
@@ -0,0 +1,52 @@
+#this program downloads .jpgs from an sftp server. if the file is already in the local directory,
+#the file will be downloaded only if the server contains a more up-to-date version.
+#if the file is not in the local directory, it will be downloaded
+
+import paramiko
+import os
+
+HOST = "demo.wftpserver.com"
+PORT = 2222
+USER = "demo"
+PASSWORD = "demo"
+
+#make a dictionary containing {file in cwd:last mod time} for local directory
+#will be used to search whether the remote files are already present locally
+fileNameDate = {}
+for item in os.listdir():
+    fileNameDate[item] = os.path.getmtime(item)
+
+#initiate SFTP connection
+transport = paramiko.Transport((HOST, PORT))
+transport.connect(username=USER, password=PASSWORD)
+client = paramiko.sftp_client.SFTPClient.from_transport(transport)
+
+# navigate to the download directory
+client.chdir('/download')
+
+#storing paths for the remote and local directories for use below
+dirname = client.getcwd()
+localdirname = os.getcwd()
+
+#iterate through each file on the remote server
+for file in client.listdir():
+    if ".jpg" in file:                #identify .jpg files
+        if file in fileNameDate:      #reference file name against local directory
+            print(file, "is present in local directory")
+            #only download if the server file is more recent than the local file
+            if client.stat(f"{dirname}/{file}").st_mtime > fileNameDate[file]:
+                print('downloading most up-to-date version of', file, 'from server...')
+                client.get(f"{dirname}/{file}", f"{localdirname}/{file}")
+            else:
+                print('Most up-to-date', file, 'already in current working directory.')
+        #download file if not present locally already
+        else:
+            print('Downloading', file, "...")
+            client.get(f"{dirname}/{file}", f"{localdirname}/{file}")
+
+client.close()
+
+    
+  
+
+
