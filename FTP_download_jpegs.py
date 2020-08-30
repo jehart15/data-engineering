08f4b9617171eb0f0aa1a09f48ec71c1e92b74ca
Jaynee Hart <jehart15@gmail.com>
2020-08-30 18:05:34 -0400

    Made functions. Added webscraping program.

--- FTP_download_jpegs.py
+++ FTP_download_jpegs.py
@@ -2,21 +2,32 @@
 
 from ftplib import FTP
 
-with FTP(host="demo.wftpserver.com", user="demo", passwd="demo") as client:
-    #navigate to the correct directory
-    client.cwd("download")
+def download_jpg():
+    '''This function downloads the JPGs from a specific FTP,
+    which is hardcoded into the function. Could one day take
+    host, user, and password as arguments.'''
+    
+    with FTP(host="demo.wftpserver.com", user="demo", passwd="demo") as client:
+        #navigate to the correct directory
+        client.cwd("download")
 
-    #client.nlist generates a list of the names of the directory contents
-    for file in client.nlst():
-        #narrow down the list to the target file type
-        if '.jpg' in file:
-            #generate a string that works with retrbinary, which requires
-            #RETR at the beginning of the file name to work
-            name = "RETR " + file
-            #added this so you can see it working
-            print(name)
-            #download the file, remember to use binary mode.
-            with open(file, 'wb') as f:
-                client.retrbinary(name, f.write)
+        #client.nlist generates a list of the names of the directory contents
+        for file in client.nlst():
+            #narrow down the list to the target file type
+            if '.jpg' in file:
+                #generate a string that works with retrbinary, which requires
+                #RETR at the beginning of the file name to work
+                name = "RETR " + file
+                #added this so you can see it working
+                print(name)
+                #download the file, remember to use binary mode.
+                with open(file, 'wb') as f:
+                    client.retrbinary(name, f.write)
+
+def main():
+    download_jpg()
+
+if  __name__ == '__main__':
+    main()
 
     
--- SFTP_download_jpegs.py
+++ SFTP_download_jpegs.py
@@ -10,43 +10,60 @@ PORT = 2222
 USER = "demo"
 PASSWORD = "demo"
 
-#make a dictionary containing {file in cwd:last mod time} for local directory
-#will be used to search whether the remote files are already present locally
-fileNameDate = {}
-for item in os.listdir():
-    fileNameDate[item] = os.path.getmtime(item)
-
-#initiate SFTP connection
-transport = paramiko.Transport((HOST, PORT))
-transport.connect(username=USER, password=PASSWORD)
-client = paramiko.sftp_client.SFTPClient.from_transport(transport)
-
-# navigate to the download directory
-client.chdir('/download')
-
-#storing paths for the remote and local directories for use below
-dirname = client.getcwd()
-localdirname = os.getcwd()
-
-#iterate through each file on the remote server
-for file in client.listdir():
-    if ".jpg" in file:                #identify .jpg files
-        if file in fileNameDate:      #reference file name against local directory
-            print(file, "is present in local directory")
-            #only download if the server file is more recent than the local file
-            if client.stat(f"{dirname}/{file}").st_mtime > fileNameDate[file]:
-                print('downloading most up-to-date version of', file, 'from server...')
-                client.get(f"{dirname}/{file}", f"{localdirname}/{file}")
-            else:
-                print('Most up-to-date', file, 'already in current working directory.')
-        #download file if not present locally already
-        else:
-            print('Downloading', file, "...")
-            client.get(f"{dirname}/{file}", f"{localdirname}/{file}")
-
-client.close()
-
+def dirContents_and_ModDate():
+    '''make a dictionary containing {file in cwd:last mod time}
+    for local directory; will be used later to search whether
+    the remote SFTP files are already present locally'''
     
+    fileNameDate = {}
+    
+    for item in os.listdir():
+        fileNameDate[item] = os.path.getmtime(item)
+    return fileNameDate
+
+def SFTP_get_jpegs(fileNameDate):
+    '''This function will download all the .jpeg files from
+    a download directory from the remote SFTP server. If the
+    same file is present locally, the file will only be
+    downloaded if the remote file is more recently modified
+    than the local one. Argument is a dictionary of the
+    local directory {'file name': mtime} '''
+
+    #initiate SFTP connection
+    transport = paramiko.Transport((HOST, PORT))
+    transport.connect(username=USER, password=PASSWORD)
+    client = paramiko.sftp_client.SFTPClient.from_transport(transport)
+
+    # navigate to the download directory
+    client.chdir('/download')
+
+    #storing paths for the remote and local directories for use below
+    dirname = client.getcwd()
+    localdirname = os.getcwd()
+
+    #iterate through each file on the remote server
+    for file in client.listdir():
+        if ".jpg" in file:                #identify .jpg files
+            if file in fileNameDate:      #reference file name against local directory
+                print(file, "is present in local directory")
+                #only download if the server file is more recent than the local file
+                if client.stat(f"{dirname}/{file}").st_mtime > fileNameDate[file]:
+                    print('downloading most up-to-date version of', file, 'from server...')
+                    client.get(f"{dirname}/{file}", f"{localdirname}/{file}")
+                else:
+                    print('Most up-to-date', file, 'already in current working directory.')
+            #download file if not present locally already
+            else:
+                print('Downloading', file, "...")
+                client.get(f"{dirname}/{file}", f"{localdirname}/{file}")
+
+    client.close()
+
+def main():
+    SFTP_get_jpegs(dirContents_and_ModDate())
+
+if __name__ == '__main__':
+    main()
   
 
 
